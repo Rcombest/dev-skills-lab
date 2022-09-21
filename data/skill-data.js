867b678c87e0b1bd8c6a1fc48d3332a149a455c6
Ryan Combest <crcombest@gmail.com>
2022-09-21 18:35:29 -0500

    adds skills array and export

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', gotIt: true, _id: 125223},
+  {text: 'Python', gotIt: false, _id: 127904},
+  {text: 'MERN', gotIt: false, _id: 139608}, 
+]
+
+export {
+  skills
+}
